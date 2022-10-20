0901ac7daa69f8d6dfb02e1217d166ed24de7145
Edu <bryanedu8@gmail.com>
2022-10-20 18:06:20 +0200

    funcion 3 y 4
    Co-Authored-By: StevenPerez13 <37144165+StevenPerez13@users.noreply.github.com>

--- Practicas Parejas/Practica 2/taskcode.js
+++ Practicas Parejas/Practica 2/taskcode.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let listaTareas = [
-    {text: "Preparar prácticas AW", tags:["universidad","awt"]}
+    {text: "Preparar prácticas AW", tags:["universidad","practica"]}
     , {text: "Mirar fechas congreso", done: true, tags:[]}
     , {text: "Ir al supermercado", tags: ["personal", "básico"]}
     , {text: "Jugar al fútbol", done: false, tags:["personal", "deportes"]}
@@ -33,13 +33,13 @@ function getToDoTasks(taskslist) {
 
 	return filtered;
 }
-console.log(getToDoTasks(listaTareas));
+// console.log(getToDoTasks(listaTareas));
 
 
 /* 2.
 Esta función devuelve un array que contiene las tareas del array tasks que contengan, en su lista
 de etiquetas, la etiqueta pasada como segundo parámetro.
-*/
+*/+
 function findByTag(taskslist, tag){
     let localed = taskslist.filter(function(tarea) {
         return tarea.tags.includes(tag);
@@ -47,7 +47,7 @@ function findByTag(taskslist, tag){
 
 	return localed;
 }
-console.log(findByTag(listaTareas, "personal"));
+// console.log(findByTag(listaTareas, "personal"));
 
 /* 3.
 Esta función devuelve un array que contiene aquellas tareas del array tasks que contengan al
@@ -55,21 +55,31 @@ menos una etiqueta que coincida con una de las del array tags pasado como segund
 */
 function findByTags(taskslist, tags) {
     let localed = taskslist.filter(function(tarea) {
-        // return tarea.tags.includes.some(tags);
+		return tarea.tags.some(r => tarea.tags.includes(r));
 	});
 
 	return localed;
 }
-console.log(findByTags(listaTareas, ["personal", "practica"]));
+// console.log(findByTags(listaTareas, ["personal", "practica"]));
 
 /* 4.
 Esta función devuelve el número de tareas completadas en el array de tareas tasks pasado como
 parámetro.
 */
 function countDone(taskslist) {
+	let result = taskslist.map(function(tarea){
+		if(tarea.done){
+			return tarea.text;
+		}
+	});
 
-}
-console.log(countDone(listaTareas));
+	let filtered = result.filter(function(tarea) {
+		 return tarea !== undefined;
+	});
+
+   return filtered.reduce((ac,n) => ac+1, 0);
+} 
+// console.log(countDone(listaTareas));
 
 /* 5.
 Esta función recibe un texto intercalado con etiquetas, cada una de ellas formada por una serie de
@@ -78,7 +88,9 @@ con su array de etiquetas extraídas de la cadena texto. Por otra parte, el atri
 resultante no debe contener las etiquetas de la cadena de entrada ni espacios en blanco de más.
 */
 function createTask(texto) {
-
+	let newText = palabras.filter(function(array) {
+		
+	});
 }
 console.log(createTask(texto));
 
