3c080f353b6d9d2b8cd6699ea59a82dfe428f87f
StevenPerez13 <pachstevenperez13@gmail.com>
2022-11-01 16:50:48 +0100

    Parte Easy
    He programado lo easy, definidas las funciones callback y todos los metodos de los DAOs. Ahora debemos realizar bien las consultas y ver como llamar a los métodos desde la función main

--- Practicas Parejas/Practica 3/Modules/DAOTasks.js
+++ Practicas Parejas/Practica 3/Modules/DAOTasks.js
@@ -1,1 +1,93 @@
 "use strict"
+class DAOTasks {
+    constructor(pool) { 
+        this.pool = pool;
+     }
+    getAllTasks(email, callback) { 
+        this.pool.getConnection(function (err,connection){
+            if (err) {
+                callback(new Error("Error de conexión a la base de datos"));
+            }
+            else {
+                const sql = "SELECT * FROM aw_tareas_usuarios WHERE email = ? AND password = ?";
+                connection.query( sql,
+                    [email], 
+                    function(err, tasks) {
+                        connection.release(); // devolver al pool la conexión
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos"));
+                        }
+                        else{
+                           callback(null,tasks);
+                        }
+                    });
+            }
+        });
+    }
+    insertTask(email, task, callback) {  
+        this.pool.getConnection(function (err,connection){
+            if (err) {
+                callback(new Error("Error de conexión a la base de datos"));
+            }
+            else { //Debemos hacer dos connection.query?
+                const sql = "SELECT * FROM aw_tareas_usuarios WHERE email = ? AND password = ?";
+                connection.query( sql,
+                    [email], 
+                    function(err, tasks) {
+                        connection.release(); // devolver al pool la conexión
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos"));
+                        }
+                        else{
+                            callback(null);
+                        }
+                    });
+            }
+        });
+    }
+    markTaskDone(idTask, callback) { 
+        this.pool.getConnection(function (err,connection){
+            if (err) {
+                callback(new Error("Error de conexión a la base de datos"));
+            }
+            else {
+                const sql = "SELECT * FROM aw_tareas_usuarios WHERE email = ? AND password = ?";
+                connection.query( sql,
+                    [idTask], 
+                    function(err, tasks) {
+                        connection.release(); // devolver al pool la conexión
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos"));
+                        }
+                        else{
+                            callback(null);
+                        }
+                    });
+            }
+        });
+    }
+    deleteCompleted(email, callback) { 
+        this.pool.getConnection(function (err,connection){
+            if (err) {
+                callback(new Error("Error de conexión a la base de datos"));
+            }
+            else {
+                const sql = "SELECT * FROM aw_tareas_usuarios WHERE email = ? AND password = ?";
+                connection.query( sql,
+                    [email], 
+                    function(err, tasks) {
+                        connection.release(); // devolver al pool la conexión
+                        if (err) {
+                            callback(new Error("Error de acceso a la base de datos"));
+                        }
+                        else{
+                            callback(null);
+                        }
+                    });
+            }
+        });
+    }
+
+}
+    
+module.exports = DAOTasks;
