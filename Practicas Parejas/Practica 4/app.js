36fd24ecb806c1f87683862757adcec31eb2adb0
Edu <bryanedu8@gmail.com>
2022-11-13 20:57:01 +0100

    intento

--- Practicas Parejas/Practica 4/app.js
+++ Practicas Parejas/Practica 4/app.js
@@ -10,12 +10,31 @@ const fs = require("fs");
 // Crear un servidor Express.js
 const app = express();
 // Crear un pool de conexiones a la base de datos de MySQL
-const pool = mysql.createPool(config.mysqlConfig);
+// const pool = mysql.createPool(config.mysqlConfig);
 // Crear una instancia de DAOTasks
-const daoT = new DAOTasks(pool);
+// const daoT = new DAOTasks(pool);
 //middleware static se 2añade a la página web
-const ficheroEstaticos = path.join(__dirname, "public");
-app.use(express.static(ficheroEstaticos));
+// const ficheroEstaticos = path.join(__dirname, "public");
+// app.use(express.static(ficheroEstaticos));
+
+//ejs
+app.set("view engine", "ejs"); // Configuracion de ejs como motor de plantilas
+
+/* //Definimos directorio de plantillas:
+const dirPlantilla = path.join(__dirname, "views");
+app.set("views", dirPlantilla); */
+
+
+/* var usuarios = ["Elver Galarga", "Elver Gotas", "Mari Flor"]; */
+
+app.get("/", function (request, reponse) {
+    // reponse.status(200);
+    reponse.render("prueba");
+});
+
+
+
+
 // Arrancar el servidor
 app.listen(config.port, function(err) {
  if (err) {
