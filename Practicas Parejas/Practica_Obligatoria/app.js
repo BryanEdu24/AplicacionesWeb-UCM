2786d0db958dad96e6f1e9553bce0e0206829aea
StevenPerez13 <pachstevenperez13@gmail.com>
2022-11-20 12:35:10 +0100

    Funciona el post del Login
    Arreglado el error de ayer, cuando te vea te lo explico más detallado

--- Practicas Parejas/Practica_Obligatoria/app.js
+++ Practicas Parejas/Practica_Obligatoria/app.js
@@ -1,105 +1,29 @@
 const config = require("./config");
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require("express-session");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-//las flash no pueden funcionar sin sesiones
-/* function flashMiddleware(request, response, next){
-  response.setFlash = function(msg){
-    request.session.flashMsg = msg;
-  };
-
-  response.locals.getAndClearFlash = function(){
-    let msg = request.session.flashMsg;
-    delete request.session.flashMsg;
-    return msg;
-  };
-
-  next();
-}
- */
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//las flash no pueden funcionar sin sesiones
-/* app.use(flashMiddleware); */
-app.use((request, response, next) => {
-  response.setFlash = (str) => {
-      request.session.flashMessage = str;
-  }
-  response.locals.getFlash = () => {
-      let mensaje = request.session.flashMessage;
-      delete request.session.flashMessage;
-      return mensaje;
-  }
-  next();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/procesar_post.html", (request, response) => {
+      console.log(request.body);
+      response.render("index", {
+        correo: request.body.correo,
+        contrasenia: request.body.contrasenia
+      });
+      response.end();
 });
 
-/*
- * Middleware para la carga del atributo request.session 
- */
-app.use(session({
-  secret: "claveuser",
-  resave: false,
-  saveUninitialized: false
-}));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-/*---------- MÉTODOS POST ----------*/
-app.post("/loginView", (request, response) => {
-  console.log("Entramos loggeado");
-  try {
-    if( request.body.usuario === ""){
-      response.setFlash("No has especificado ninguna dato");
-    }else{
-      response.setFlash("Has especificado datos");
-      let usuario = {
-        correo: request.body.usuario.correo,
-        contraseña: request.body.usuario.contraseña
-      }
-      console.log(usuario);
-    }
-
-  } catch(error) {
-    response.render("index", {usuarios: usuario});
-  }
-
-});
 
 app.listen(config.port, function(err) {
   if (err) {
@@ -110,4 +34,3 @@ app.listen(config.port, function(err) {
   }
  });
 
-module.exports = app;
